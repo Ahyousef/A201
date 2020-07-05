5d0e8bf6482d25d081120711da3afa0b16bae72e
abdallahshanaah <abdallah_m_sh@yahoo.com>
2020-07-05 12:56:39 +0300

    new

---
+++ script.js
@@ -0,0 +1,10 @@
+"strict use only"
+
+var name = prompt("What is your name?")
+var birthYear = prompt("Which year were you born in?")
+var favFood = prompt("What is your favorite food?")
+var favDrink = prompt("What is your favorite drink?")
+
+var age = 2020-birthYear
+
+alert("Hello " + name + ", this year you turn " + age + ". Your favorite food is " + favFood + ", and your favorite drink is "+ favDrink + ".")
